2cca0b1cc5833a90eca6709aa19a6901db23d951
seung-hun2 <jungsh2328@naver.com>
2023-05-07 23:47:26 +0900

    feat: 신고 도메인 조회 이외 Mock API 생성

--- comment-service/src/main/java/com/blockpage/commentservice/adaptor/web/ReportController.java
+++ comment-service/src/main/java/com/blockpage/commentservice/adaptor/web/ReportController.java
@@ -9,8 +9,10 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +24,7 @@ public class ReportController {
     @PostMapping("")
     public ResponseEntity report(@RequestBody RequestReport requestReport) {
         // 신고하는 서비스 로직 구현 해야함
-        return ResponseEntity.status(HttpStatus.CREATED).body("해당 댓글이 신고 되었습니다.");
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponseView<>("해당 댓글이 신고 되었습니다."));
     }
 
     @GetMapping("")
@@ -33,6 +35,20 @@ public class ReportController {
         reportViewList.add(new ReportView(3L, "부적절한사람3", "매우 노잼", Date.valueOf("2023-05-06"), ReportType.ABUSE));
 
         return ResponseEntity.status(HttpStatus.OK).body(new ApiResponseView<>(reportViewList));
+    }
+
+    @PutMapping
+    public ResponseEntity acceptReport(@RequestBody RequestReport requestReport){
+        // 신고 승인되는 서비스 로직 구현해야함
+        // 해당 댓글의 report ( true 로 변경 ), 해당 ReportEntity 삭제 해야함
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponseView<>("해당 댓글 신고처리가 승인되어, 삭제되어집니다."));
+    }
+
+    @DeleteMapping
+    public ResponseEntity denyReport(@RequestBody RequestReport requestReport){
+        // 신고 거부되는 서비스 로직 구현해야함
+        // 해당 reportEntity 삭제
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponseView<>("해당 댓글 신고처리가 거부되어, 삭제되어집니다."));
     }
 
 }
