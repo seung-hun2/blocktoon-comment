37d29a55f86e4681bdbb097bf7b92c97aa7a4d1b
seung-hun2 <jungsh2328@naver.com>
2023-05-11 17:08:39 +0900

    feat: 댓글 Entity 수정

--- comment-service/src/main/java/com/blockpage/commentservice/adaptor/infrastructure/entity/CommentEntity.java
+++ comment-service/src/main/java/com/blockpage/commentservice/adaptor/infrastructure/entity/CommentEntity.java
@@ -1,15 +1,22 @@
 package com.blockpage.commentservice.adaptor.infrastructure.entity;
 
+import com.blockpage.commentservice.domain.CommentDomain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(schema = "comment")
 public class CommentEntity {
 
@@ -40,4 +47,20 @@ public class CommentEntity {
     private int replyCount;
     @Column
     private Boolean pin;
+
+    public static CommentEntity toEntityFromDomain(CommentDomain commentDomain){
+        return CommentEntity.builder()
+            .episodeId(commentDomain.getEpisodeId())
+            .childId(commentDomain.getChildId())
+            .childNickname(commentDomain.getChildNickname())
+            .parentsId(commentDomain.getParentsId())
+            .parentsNickname(commentDomain.getParentsNickname())
+            .content(commentDomain.getComment())
+            .likesCount(commentDomain.getLikesCount())
+            .dislikesCount(commentDomain.getDislikesCount())
+            .report(commentDomain.getReport())
+            .erase(commentDomain.getErase())
+            .pin(commentDomain.getPin())
+            .build();
+    }
 }
