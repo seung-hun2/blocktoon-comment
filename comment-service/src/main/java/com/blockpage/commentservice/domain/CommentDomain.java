be73beac2ad836afc6fbfd91d152cc8eb7d2ccfc
seung-hun2 <jungsh2328@naver.com>
2023-05-12 11:41:01 +0900

    fix: 댓글 도메인 수정

--- comment-service/src/main/java/com/blockpage/commentservice/domain/CommentDomain.java
+++ comment-service/src/main/java/com/blockpage/commentservice/domain/CommentDomain.java
@@ -8,26 +8,28 @@ import lombok.Getter;
 @Builder
 public class CommentDomain {
 
+    private Long commentId;
     private Long episodeId;
     private Long parentsId;
     private String parentsNickname;
     private Long childId;
     private String childNickname;
-    private String comment;
+    private String content;
     private int likesCount;
     private int dislikesCount;
     private Boolean report;
     private Boolean erase;
     private Boolean pin;
 
-    public static CommentDomain toDomainFrom(CommentQuery commentQuery){
+    public static CommentDomain toDomainFrom(CommentQuery commentQuery) {
         return new CommentDomain(
+            commentQuery.getCommentId(),
             commentQuery.getEpisodeId(),
             commentQuery.getParentsId(),
             commentQuery.getParentsNickname(),
             commentQuery.getChildId(),
             commentQuery.getChildNickname(),
-            commentQuery.getComment(),
+            commentQuery.getContent(),
             commentQuery.getLikesCount(),
             commentQuery.getDislikesCount(),
             commentQuery.getReport(),
