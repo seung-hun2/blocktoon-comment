5f9cde5f636a47558971e871184c7431fe3d0e50
seung-hun2 <jungsh2328@naver.com>
2023-05-12 11:42:26 +0900

    feat: 댓글 도메인 유스케이스 구현체 생성

--- comment-service/src/main/java/com/blockpage/commentservice/application/service/CommentService.java
+++ comment-service/src/main/java/com/blockpage/commentservice/application/service/CommentService.java
@@ -4,6 +4,8 @@ import com.blockpage.commentservice.application.port.SaveCommentDto;
 import com.blockpage.commentservice.application.port.in.CommentUseCase;
 import com.blockpage.commentservice.application.port.out.CommentPort;
 import com.blockpage.commentservice.domain.CommentDomain;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +23,48 @@ public class CommentService implements CommentUseCase {
 
         return SaveCommentDto.toDtoFromQuery(commentQuery);
 
+    }
+
+    @Override
+    public SaveCommentDto pinComment(CommentQuery commentQuery) {
+
+        CommentDomain commentDomain = CommentDomain.toDomainFrom(commentQuery);
+        commentPort.pinComment(commentDomain);
+
+        return SaveCommentDto.toDtoFromQuery(commentQuery);
+    }
+
+    @Override
+    public SaveCommentDto deleteComment(CommentQuery commentQuery) {
+
+        CommentDomain commentDomain = CommentDomain.toDomainFrom(commentQuery);
+        commentPort.deleteComment(commentDomain);
+
+        return SaveCommentDto.toDtoFromQuery(commentQuery);
+    }
+
+    @Override
+    public List<SaveCommentDto> getComment(CommentQuery commentQuery) {
+
+        CommentDomain commentDomain = CommentDomain.toDomainFrom(commentQuery);
+        List<CommentQuery> commentQueryList = commentPort.getComment(commentDomain);
+
+        List<CommentQuery> result = commentQueryList.stream()
+            .filter(w -> !(w.getErase() && w.getReplyCount() == 0)).toList();
+
+        return result.stream().map(SaveCommentDto::toDtoFromQuery).collect(Collectors.toList());
+
+    }
+
+    @Override
+    public List<SaveCommentDto> getReply(CommentQuery commentQuery) {
+
+        CommentDomain commentDomain = CommentDomain.toDomainFrom(commentQuery);
+        List<CommentQuery> commentQueryList = commentPort.getReply(commentDomain);
+
+        List<CommentQuery> result = commentQueryList.stream()
+            .filter(w -> !(w.getErase() && w.getReplyCount() == 0)).toList();
+
+        return result.stream().map(SaveCommentDto::toDtoFromQuery).collect(Collectors.toList());
     }
 }
