7834f80897256faabe0d67c13c0b4f9a9fce4560
seung-hun2 <jungsh2328@naver.com>
2023-05-16 12:13:50 +0900

    feat: 신고 도메인 레포 생성, PORT 생성 및 구현체 생성

---
+++ comment-service/src/main/java/com/blockpage/commentservice/adaptor/infrastructure/persistence/ReportAdaptor.java
@@ -0,0 +1,37 @@
+package com.blockpage.commentservice.adaptor.infrastructure.persistence;
+
+import com.blockpage.commentservice.adaptor.infrastructure.entity.ReportEntity;
+import com.blockpage.commentservice.adaptor.infrastructure.repository.ReportRepository;
+import com.blockpage.commentservice.application.port.out.ReportPort;
+import com.blockpage.commentservice.domain.ReportDomain;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ReportAdaptor implements ReportPort {
+
+    private final ReportRepository reportRepository;
+
+    @Override
+    public void saveReport(ReportDomain reportDomain) {
+        ReportEntity report = ReportEntity.toEntityFromDomain(reportDomain);
+        reportRepository.save(report);
+    }
+
+    @Override
+    @Transactional
+    public void solveReport(ReportDomain reportDomain) {
+        reportRepository.deleteAllByCommentId(reportDomain.getCommentId());
+    }
+
+    @Override
+    public List<ReportDomain> getReport(Long commentId) {
+
+        List<ReportEntity> reportEntityList = reportRepository.findAll();
+
+        return reportEntityList.stream().map(ReportDomain::toDomainFromEntity).toList();
+    }
+}
