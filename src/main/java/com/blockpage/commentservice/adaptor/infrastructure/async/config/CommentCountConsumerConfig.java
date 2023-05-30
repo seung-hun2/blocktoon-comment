9aad8c157ac3e693d48754deee6290fe576caefd
seung-hun2 <jungsh2328@naver.com>
2023-05-30 10:50:51 +0900

    feat: 댓글반응 업데이트를 위한 kafka consumer 설정파일 생성

---
+++ src/main/java/com/blockpage/commentservice/adaptor/infrastructure/async/config/CommentCountConsumerConfig.java
@@ -0,0 +1,53 @@
+package com.blockpage.commentservice.adaptor.infrastructure.async.config;
+
+
+import com.blockpage.commentservice.adaptor.infrastructure.async.message.CommentCountMessage;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+@EnableKafka
+@Configuration
+public class CommentCountConsumerConfig {
+
+    @Value("${spring.kafka.bootstrapAddress}")
+    private String bootStrapServer;
+
+    @Value("${spring.kafka.likeGroup}")
+    private String groupName;
+
+    @Bean
+    public ConsumerFactory<String, CommentCountMessage> ConsumerFactory() {
+        JsonDeserializer<CommentCountMessage> deserializer = new JsonDeserializer<>(CommentCountMessage.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeMapperForKey(true);
+
+        return new DefaultKafkaConsumerFactory<>(consumerFactoryConfig(deserializer), new StringDeserializer(), deserializer);
+    }
+
+    private Map<String, Object> consumerFactoryConfig(JsonDeserializer<CommentCountMessage> deserializer) {
+        HashMap<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupName);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+        return props;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, CommentCountMessage> KafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, CommentCountMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(ConsumerFactory());
+        return factory;
+    }
+}
