964acef380852507875bd1b5ce21070a7b692b40
seung-hun2 <jungsh2328@naver.com>
2023-05-30 10:51:18 +0900

    feat: 댓글반응 업데이트를 위한 kafka message listener 생성

---
+++ src/main/java/com/blockpage/commentservice/adaptor/infrastructure/async/message/CommentCountMessageListener.java
@@ -0,0 +1,26 @@
+package com.blockpage.commentservice.adaptor.infrastructure.async.message;
+
+import com.blockpage.commentservice.application.port.in.CommentCountUseCase;
+import com.blockpage.commentservice.application.port.in.CommentCountUseCase.CommentCountQuery;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommentCountMessageListener {
+
+    private final CommentCountUseCase commentCountUseCase;
+
+    @KafkaListener(
+        topics = "${spring.kafka.commentTopic}",
+        groupId = "${spring.kafka.commentGroup}",
+        containerFactory = "dislikeKafkaListenerContainerFactory"
+    )
+    public void listenWithHeaders(@Payload CommentCountMessage commentCountMessage, @Headers MessageHeaders messageHeaders) {
+        commentCountUseCase.updateComment(CommentCountQuery.toQuery(commentCountMessage));
+    }
+}
